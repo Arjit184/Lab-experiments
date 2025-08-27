543c74817ae002d74a9719f69ef35f3ff21f58c8
Arjit184 <arjitpathak805@gmail.com>
2025-08-27 12:26:50 +0530

    lab1

--- exp1/name.c
+++ exp1/name.c
@@ -1,13 +1,16 @@
 //Write a program that prompts the user to enter their name and age. 
 
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    char name;
+    char name;  
     int age;
-    printf("Enter your name and age \n");
-    scanf("%s",&name);
-    scanf("%d",&age);
-    printf("Name=%s \n",name);
-    printf("Age=%d",age);
+    printf("Enter your name  and age:\n");
+    scanf(" %c", &name); 
+    scanf("%d", &age);
+    printf("Name = %c\n", name);
+    printf("Age = %d\n", age);
+
+    return 0;
 }
